c6c0a4aaaad0ddfcfa7c0eb2cba1891fca9dc2e3
paniq123321 <ecepaniq@gmail.com>
2025-06-03 14:34:08 -0300

    few fix

--- components/brands.tsx
+++ components/brands.tsx
@@ -4,7 +4,7 @@ export function Brands() {
   return (
     <section className="pt-[7rem] pb-[1rem] ">
       <h2 className="mb-[4rem] text-[1.5rem] text-center">Бренды которые мы продаем</h2>
-      <div className="grid grid-cols-[repeat(4,max-content)] gap-x-[1.25rem] max-w-[1024px] justify-center m-auto">
+      <div className="grid grid-cols-[repeat(2,max-content)] sm:grid-cols-[repeat(4,max-content)] gap-x-[2.5rem] sm:gap-x-[1.25rem] max-w-[1024px] justify-center m-auto">
         <Image
           width={120}
           height={120}
